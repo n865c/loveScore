99f65f82290b22904f657d26ec551c0bbcdbae88
Nancy Kumari <66416141+n865c@users.noreply.github.com>
2022-08-25 21:18:13 +0530

    Add files via upload

---
+++ loveScore.js
@@ -0,0 +1,5 @@
+var name2=prompt("what is your name");
+var name1=prompt("What is their name");
+var loveScore=Math.random()*100;
+loveScore=Math.floor(loveScore)+1;
+alert("love score of "+name2+" and "+name1+" is "+loveScore+"%");
